9c699ad96cae92fa43c627769935ec9d971b86d2
Muhammad Ibrahim <muhammadibrahimmanofgod@gmail.com>
2025-08-27 00:28:54 +0100

    Update signup message: confirmation email sent

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ import { ReferralPopup } from "@/components/ReferralPopup";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-white overflow-x-hidden">
       <Navbar />
       <Hero />
       <FeaturedProducts />
